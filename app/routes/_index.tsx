44109a2ed94cf6c22b2a2d5e7e08c99d6dd13f27
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2025-03-25 13:21:32 +0100

    update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,18 @@
-import { Button, Card, Title1, Body1 } from '@fluentui/react-components';
+import {
+  Button,
+  Card,
+  Title1,
+  Body1,
+  Combobox,
+  Option,
+  Dialog,
+  DialogTrigger,
+  DialogSurface,
+  DialogBody,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from '@fluentui/react-components';
 import { BookmarkRegular } from '@fluentui/react-icons';
 
 export default function Index() {
@@ -9,6 +23,35 @@ export default function Index() {
       <Button appearance="primary" icon={<BookmarkRegular />}>
         Click me
       </Button>
+      <Combobox placeholder="Select an animal">
+        {['Dog', 'Cat', 'Ferret'].map((option) => (
+          <Option key={option} disabled={option === 'Ferret'}>
+            {option}
+          </Option>
+        ))}
+      </Combobox>
+      <Dialog>
+        <DialogTrigger disableButtonEnhancement>
+          <Button>Open dialog</Button>
+        </DialogTrigger>
+        <DialogSurface>
+          <DialogBody>
+            <DialogTitle>Dialog title</DialogTitle>
+            <DialogContent>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              exercitationem cumque repellendus eaque est dolor eius expedita
+              nulla ullam? Tenetur reprehenderit aut voluptatum impedit
+              voluptates in natus iure cumque eaque?
+            </DialogContent>
+            <DialogActions>
+              <Button appearance="primary">Do Something</Button>
+              <DialogTrigger disableButtonEnhancement>
+                <Button appearance="secondary">Close</Button>
+              </DialogTrigger>
+            </DialogActions>
+          </DialogBody>
+        </DialogSurface>
+      </Dialog>
     </Card>
   );
 }
